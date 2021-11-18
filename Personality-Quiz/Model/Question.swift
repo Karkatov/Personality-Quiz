17722011a6d51607ebdf8d6e476dbf4d765fc7eb
Duxxless <Karkatof@ya.ru>
2021-11-18 16:40:40 +0300

    Files are decomposed on folders

--- Personality-Quiz/Model/Question.swift
+++ Personality-Quiz/Model/Question.swift
@@ -21,7 +21,27 @@ struct Question {
                     Answer(text: "Морковь", type: .rabbit),
                     Answer(text: "Кукуруза", type: .turtle)
                 ]
-            )
+            ),
+            Question(
+                text: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(text: "Плавать", type: .dog),
+                    Answer(text: "Спать", type: .cat),
+                    Answer(text: "Обниматься", type: .rabbit),
+                    Answer(text: "Есть", type: .turtle)
+                    
+                ]
+            ),
+            Question(
+                text: "Любители вы поездки на машине?",
+                type: .ranged,
+                answers: [
+                    Answer(text: "Ненавижу", type: .dog),
+                    Answer(text: "Нервничаю", type: .cat),
+                    Answer(text: "Не замечаю", type: .rabbit),
+                    Answer(text: "Обожаю", type: .turtle)  ]
+            ),
         ]
     }
 }
